501f45fe7a5a5e9db3cbae72773671553a553276
kasperiapell <kasperi.apell@gmail.com>
2022-06-22 22:34:36 +0300

    initial commit

---
+++ sorting/exchange_sort/exchange_sort.py
@@ -0,0 +1,9 @@
+def exchange_sort(a):
+    for i in range(len(a)-1):
+        for j in range(i+1,len(a)):
+            x = a[i]
+
+            if x > a[j]:
+                a[i] = a[j]
+                a[j] = x
+    return a
