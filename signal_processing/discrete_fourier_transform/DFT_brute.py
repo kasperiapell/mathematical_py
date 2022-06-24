8646a534f7fb4191a9d6833723126b602a08ed94
kasperiapell <kasperi.apell@gmail.com>
2022-06-24 08:07:41 +0300

    initial commit

---
+++ signal_processing/discrete_fourier_transform/DFT_brute.py
@@ -0,0 +1,14 @@
+import math
+
+def DFT_brute (z):
+	L = len(z)
+	R = range(L)
+	w = [0 for i in R]
+	Z = [[0] * L for i in R]
+
+	for n in R:
+		for m in R:
+			arg = (2 * math.pi * n * m) / L
+			Z[n][m] = z[n] * (math.cos(arg) - complex(0, math.sin(arg)))
+		w[n] = sum(Z[n])
+	return w
